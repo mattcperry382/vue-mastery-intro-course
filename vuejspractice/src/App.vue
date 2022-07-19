afc375cf382b514e95e8bef80ba631420aa82c26
mattcperry382 <mattcperry382@gmail.com>
2022-07-18 22:02:05 -0600

    commit other files

---
+++ vuejspractice/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <hello-world></hello-world>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+const componentName = 'app';
+
+export default {
+  components: { HelloWorld },
+  name: componentName
+}
+</script>
+
+<style>
+
+</style>
